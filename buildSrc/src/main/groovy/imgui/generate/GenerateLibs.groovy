17d2cb85c6cc642b9e723043282621e53941d8e0
Danil Dubrovksy <forwot163@gmail.com>
2021-01-05 16:45:45 +0300

    Added simple icon drawing (from imgui-node-editor samples). Added more bindings for imgui-node-editor

--- buildSrc/src/main/groovy/imgui/generate/GenerateLibs.groovy
+++ buildSrc/src/main/groovy/imgui/generate/GenerateLibs.groovy
@@ -43,6 +43,8 @@ class GenerateLibs extends DefaultTask {
 
             spec.from(project.rootProject.file('imnodes')) { CopySpec it -> it.include('*.h', '*.cpp') }
 
+            spec.from(project.rootProject.file('imgui-node-editor')) { CopySpec it -> it.include('*.h', '*.cpp', '*.inl') }
+
             if (withFreeType) {
                 spec.from(project.rootProject.file('imgui/misc/freetype')) { CopySpec it -> it.include('*.h', '*.cpp') }
             }
--- imgui-binding/src/main/java/imgui/ImNodeEditor.java
+++ imgui-binding/src/main/java/imgui/ImNodeEditor.java
@@ -63,6 +63,7 @@ public final class ImNodeEditor {
         #include <imgui_node_editor.h>
         #include "jni_common.h"
         #include "jni_binding_struct.h"
+        #include "widgets.h"
         namespace ed = ax::NodeEditor;
      */
 
@@ -156,22 +157,92 @@ public final class ImNodeEditor {
 
     // ADDITIONAL LOGIC
 
-    public static native boolean showNodeContextMenu(int nodeId); /*
-        return ed::ShowNodeContextMenu((ed::NodeId*)nodeId);
+    public static native long getDoubleClickedNode(); /*
+        return (jlong)(uintptr_t)ed::GetDoubleClickedNode();
     */
 
-    public static native boolean showPinContextMenu(int pinId); /*
-        return ed::ShowPinContextMenu((ed::PinId*)pinId);
+    public static native long getDoubleClickedPin(); /*
+        return (jlong)(uintptr_t)ed::GetDoubleClickedPin();
     */
 
-    public static native boolean showLinkContextMenu(int linkId); /*
-        return ed::ShowLinkContextMenu((ed::LinkId*)linkId);
+    public static native long getDoubleClickedLink(); /*
+        return (jlong)(uintptr_t)ed::GetDoubleClickedLink();
+    */
+
+    public static native float getCurrentZoom(); /*
+        return ed::GetCurrentZoom();
+    */
+
+    public static native void pinRect(float x, float y, float w, float h); /*
+        ed::PinRect(ImVec2(x, y), ImVec2(w, h));
+    */
+
+    public static native void pinPivotRect(float x, float y, float w, float h); /*
+        ed::PinPivotRect(ImVec2(x, y), ImVec2(w, h));
+    */
+
+    public static native void pinPivotSize(float w, float h); /*
+        ed::PinPivotSize(ImVec2(w, h));
+    */
+
+    public static native void pinPivotScale(float w, float h); /*
+        ed::PinPivotScale(ImVec2(w, h));
+    */
+
+    public static native void pinPivotAlignment(float x, float y); /*
+        ed::PinPivotAlignment(ImVec2(x, y));
+    */
+
+    public static boolean showNodeContextMenu(ImInt nodeId){
+        return nShowNodeContextMenu(nodeId.getData());
+    }
+
+    public static boolean showPinContextMenu(ImInt pinId){
+        return nShowPinContextMenu(pinId.getData());
+    }
+
+    public static boolean showLinkContextMenu(ImInt linkId){
+        return nShowLinkContextMenu(linkId.getData());
+    }
+
+    public static native long getNodeWithContextMenu(); /*
+        ed::NodeId id;
+        return ed::ShowNodeContextMenu(&id) ? (jlong)(uintptr_t)id : -1;
+    */
+
+    public static native long getPinWithContextMenu(); /*
+        ed::PinId id;
+        return ed::ShowPinContextMenu(&id) ? (jlong)(uintptr_t)id : -1;
+    */
+
+    public static native long getLinkWithContextMenu(); /*
+        ed::LinkId id;
+        return ed::ShowLinkContextMenu(&id) ? (jlong)(uintptr_t)id : -1;
+    */
+
+    private static native boolean nShowNodeContextMenu(int[] nodeId); /*
+        return ed::ShowNodeContextMenu((ed::NodeId*)&nodeId[0]);
+    */
+
+    private static native boolean nShowPinContextMenu(int[] pinId); /*
+        return ed::ShowPinContextMenu((ed::PinId*)&pinId[0]);
+    */
+
+    private static native boolean nShowLinkContextMenu(int[] linkId); /*
+        return ed::ShowLinkContextMenu((ed::LinkId*)&linkId[0]);
     */
 
     public static native boolean showBackgroundContextMenu(); /*
         return ed::ShowBackgroundContextMenu();
     */
 
+    public static native void suspend(); /*
+        ed::Suspend();
+    */
+
+    public static native void resume(); /*
+        ed::Resume();
+    */
 
     public static native void setNodePosition(int node, float x, float y); /*
         ed::SetNodePosition(node, ImVec2(x, y));
@@ -269,5 +340,29 @@ public final class ImNodeEditor {
     */
 
     // ADDITIONAL LOGIC END
+
+    // UTILITIES
+    // TODO: MOVE OUT OF THIS CLASS
+
+    public enum IconType {
+        Flow,
+        Circle,
+        Square,
+        Grid,
+        RoundSquare,
+        Diamond
+    }
+
+    public static void icon(IconType type, float w, float h, boolean isFilled, float r, float g, float b, float a,
+                                     float ir, float ig, float ib, float ia) {
+        nIcon(w, h, type.ordinal(), isFilled, r, g, b, a, ir, ig, ib, ia);
+    }
+
+    private static native void nIcon(float w, float h, int type, boolean isFilled, float r, float g, float b, float a,
+                                   float ir, float ig, float ib, float ia); /*
+        ax::Widgets::Icon(ImVec2(w, h), (ax::Drawing::IconType)type, isFilled, ImVec4(r, g, b, a), ImVec4(ir, ig, ib, ia));
+    */
+
+    // UTILITIES END
 
 }
