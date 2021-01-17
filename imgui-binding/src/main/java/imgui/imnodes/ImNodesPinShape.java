fab9e4bb4ebf11d4fbb3575f264ef6de7849e3c8
Danil Dubrovksy <forwot163@gmail.com>
2021-01-17 08:16:39 +0300

    Implemented demo graph editor for imnodes and imgui-node-editor. Fixed issuew ith ImNodesPinShape.

--- imgui-binding/src/main/java/imgui/imnodes/ImNodesPinShape.java
+++ imgui-binding/src/main/java/imgui/imnodes/ImNodesPinShape.java
@@ -9,9 +9,9 @@ public final class ImNodesPinShape {
     }
 
     public static final int Circle = 0;
-    public static final int CircleFilled = 2;
-    public static final int Triangle = 3;
-    public static final int TriangleFilled = 4;
-    public static final int Quad = 5;
+    public static final int CircleFilled = 1;
+    public static final int Triangle = 2;
+    public static final int TriangleFilled = 3;
+    public static final int Quad = 4;
     public static final int QuadFilled = 5;
 }
